f8017926cd4149f66c2f5bece6941d2cf237bf61
Bill Searle <billsearle@gmail.com>
2020-04-13 00:48:40 -0700

    Updated for env

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    GOOGLE_SHEETS_KEY: process.env.GOOGLE_SHEETS_KEY,
+    FIREBASE_KEY: process.env.FIREBASE_KEY,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+  },
+};
