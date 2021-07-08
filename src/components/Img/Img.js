78b0cfe0be75fa99a59262d1a1a3dbf0c63ab63b
Bill Searle <billsearle@gmail.com>
2021-07-08 10:26:34 +1000

    Revert Img changes

--- src/components/Img/Img.js
+++ src/components/Img/Img.js
@@ -1,16 +1,29 @@
+import cn from 'classnames';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Img = ({
   src, alt, className, height, width, ...rest
-}) => (
-  <Image
-    {...rest}
-    src={src}
-    alt={alt}
-    placeholder="blur"
-  />
-);
+}) => {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <Image
+      {...rest}
+      unsized={!height || !width}
+      width={width}
+      height={height}
+      src={src}
+      alt={alt}
+      quality={80}
+      className={cn(className, 'transition-opacity duration-300', {
+        'opacity-0': !loaded,
+        'opacity-100': loaded,
+      })}
+      onLoad={() => setLoaded(true)}
+    />
+  );
+};
 
 Img.propTypes = {
   src: PropTypes.string.isRequired,
